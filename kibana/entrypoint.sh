1365b3165e4e3d6eb5cefcd21b756d444d9c0fee
cloviq <cloviq@gmail.com>
2024-02-26 13:54:45 -0500

    Committing changes to kibana config

--- kibana/entrypoint.sh
+++ kibana/entrypoint.sh
@@ -3,8 +3,11 @@
 source ~/.profile
 cp /tmp/kibana.dev.yml /usr/share/kibana/config
 cd /usr/share/kibana
+ls -lah &> /usr/share/kibana/run_logs/kibana_dir.txt
 nvm use
-yarn kbn clean
-yarn cache clean
-yarn kbn bootstrap
+#yarn kbn clean
+#yarn cache clean
+#git config --global --add safe.directory /usr/share/kibana
+yarn kbn bootstrap &> run_logs/bootstrap_logs.txt
+#node scripts/kbn bootstrap
 yarn start --run-examples
